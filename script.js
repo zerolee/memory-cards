5b6b8997bc73e7eb41414fbdf5deb3563b7413dc
zerolee <zmqclee@gmail.com>
2023-02-26 00:50:11 +0800

    重新安排 script.js 中的变量、事件、函数

--- script.js
+++ script.js
@@ -26,6 +26,8 @@ const etyma_delete = document.getElementById('etyma-delete');
 const etyma_marker = document.getElementById('etyma-marker');
 const etymaFilter = document.querySelector('[name=etyma-filter]');
 
+
+
 /*****************************************************************
  ** 变量定义
  *****************************************************************/
@@ -45,14 +47,65 @@ const cardsData = getCardsData();
 // 存储用来分配的 Id
 const cardsId = getCardsId();
 
+
+
 /*****************************************************************
  ** 函数定义
  *****************************************************************/
-// Create all cards
-function createCards() {
-    cardsData.forEach((data, index) => createCard(data, index));
+////////////////////////////////////////////////////////////////
+// 数据的导入、导出，保存与恢复
+// Import other card 从外部导入
+function getExtraCardsData(input) {
+    let file = input.files[0];
+
+    let reader = new FileReader();
+
+    reader.readAsText(file);
+    let etymas;
+    reader.onload = function () {
+        etymas = JSON.parse(reader.result);
+        etymas.forEach((data) => {
+            createCard(data);
+            cardsData.push(data);
+            setCardsData(cardsData);
+        });
+    }
 }
 
+// Export cards 导出到外部
+function exportCardsData() {
+    let link = document.createElement('a');
+    link.download = 'etyma.json';
+
+    let blob = new Blob([JSON.stringify(cardsData)], {type: 'text/plain'});
+    link.href = URL.createObjectURL(blob);
+
+    link.click();
+
+    URL.revokeObjectURL(link.href);
+}
+
+// Get cards from local storage 从浏览器中恢复数据
+function getCardsData() {
+    return JSON.parse(localStorage.getItem('cards')) ?? [];
+}
+
+// Get cardsId from local storage
+function getCardsId() {
+    return JSON.parse(localStorage.getItem('cardsId')) ?? [];
+
+}
+
+// Add card to local storage 保存至浏览器中
+function setCardsData(cards) {
+    localStorage.setItem('cards', JSON.stringify(cards));
+    localStorage.setItem('cardsId', JSON.stringify(cardsId));
+    window.location.reload();
+}
+
+
+////////////////////////////////////////////////////////////////
+// Card 的创建
 // Create a single card in DOM
 function createCard(data, index) {
     const card = document.createElement('div');
@@ -164,158 +217,40 @@ function createCard(data, index) {
     updateCurrentText();
 }
 
-// Delete Card
-etyma_delete.addEventListener('click', () => {
-    if (confirm("你确定要删除当前 Card?")) {
-        cardsId.push(cardsData[currentActiveCard].id);
-        cardsContainer.removeChild(cardsEl[currentActiveCard]);
-        cardsEl.splice(currentActiveCard, 1);
-        cardsData.splice(currentActiveCard, 1);
-        if (currentActiveCard > cardsEl.length - 1) {
-            currentActiveCard = cardsEl.length - 1;
-        }
-        cardsEl[currentActiveCard].className = 'card active'
-        updateCurrentText();
-        etymaMarker(cardsData[currentActiveCard]);
-        // 存储数据，不刷新页面
-        localStorage.setItem('cards', JSON.stringify(cardsData));
-        localStorage.setItem('cardsId', JSON.stringify(cardsId));
-    }
-});
-
-
-// Edit Card
-etyma_edit.addEventListener('click', () => {
-    addContainer.classList.add('show');
-    etyma_name.value = cardsData[currentActiveCard].name;
-    etyma_value.value = cardsData[currentActiveCard].value;
-    etyma_example_name.value = cardsData[currentActiveCard].example.name;
-    etyma_example_value.value = cardsData[currentActiveCard].example.value;
-    New = false;
-    document.querySelector('#add-container>div>h2').firstChild.replaceWith('Edit Cards');
-});
-
+// Create all cards
+function createCards() {
+    cardsData.forEach((data, index) => createCard(data, index));
+}
 
 // Show number of cards
 function updateCurrentText() {
     currentEl.value = `${currentActiveCard + 1}/${cardsEl.length}`;
 }
 
-form_submit.addEventListener('submit', e => {
-    e.preventDefault();
-    let value = currentEl.value.split('/');
-    specifyCards(value[0] - 1, value[1]);
-});
 
-function specifyCards(first, second) {
-    if (first >= 0 && first < cardsEl.length && second == cardsEl.length) {
-        if (first < currentActiveCard) {
+/**
+ * 去编号为 cardNum 的 Card
+ *
+ * @param {number} cardNum 要去的 Card 的编号
+ * @param {number} Card 的总数
+ */
+//
+// gotoCard: 要去的
+function specifyCards(cardNum, cardCount) {
+    if (cardNum >= 0 && cardNum < cardsEl.length && cardCount == cardsEl.length) {
+        if (cardNum < currentActiveCard) {
             cardsEl[currentActiveCard].className = 'card right';
-        } else if (first > currentActiveCard){
+        } else if (cardNum > currentActiveCard){
             cardsEl[currentActiveCard].className = 'card left';
         }
 
-        currentActiveCard = first;
+        currentActiveCard = cardNum;
         cardsEl[currentActiveCard].className = 'card active';
         updateCurrentText();
         etymaMarker(cardsData[currentActiveCard]);
     }
 }
 
-
-// Get cards from local storage
-function getCardsData() {
-    return JSON.parse(localStorage.getItem('cards')) ?? [];
-}
-
-// Get cardsId from local storage
-function getCardsId() {
-    return JSON.parse(localStorage.getItem('cardsId')) ?? [];
-
-}
-
-// Add card to local storage
-function setCardsData(cards) {
-    localStorage.setItem('cards', JSON.stringify(cards));
-    localStorage.setItem('cardsId', JSON.stringify(cardsId));
-    window.location.reload();
-}
-
-createCards();
-
-
-/*****************************************************************
- ** 事件绑定
- *****************************************************************/
-// Event listeners
-
-// Next button
-nextBtn.addEventListener('click', () => {
-    cardsEl[currentActiveCard].className = 'card left';
-
-    currentActiveCard = currentActiveCard + 1;
-
-    if (currentActiveCard > cardsEl.length - 1) {
-        currentActiveCard = cardsEl.length - 1;
-    }
-
-    cardsEl[currentActiveCard].className = 'card active';
-
-    updateCurrentText();
-    etymaMarker(cardsData[currentActiveCard]);
-});
-
-// Prev button
-prevBtn.addEventListener('click', () => {
-    cardsEl[currentActiveCard].className = 'card right';
-
-    currentActiveCard = currentActiveCard - 1;
-
-    if (currentActiveCard < 0) {
-        currentActiveCard = 0;
-    }
-
-    cardsEl[currentActiveCard].className = 'card active';
-
-    updateCurrentText();
-    etymaMarker(cardsData[currentActiveCard]);
-});
-
-// radom cards
-etyma_radom.addEventListener('click', () => {
-    let first;
-    do {
-        first = Math.floor(Math.random()*cardsEl.length);
-    } while(cardsData[first].marker);
-    specifyCards(first, cardsEl.length);
-});
-
-// Show add container
-showBtn.addEventListener('click', () => addContainer.classList.add('show'));
-// Hide add container
-hideBtn.addEventListener('click', () => {
-    addContainer.classList.remove('show')
-    New = true;
-    etyma_name.value = '';
-    etyma_value.value = '';
-    etyma_example_name.value = '';
-    etyma_example_value.value = '';
-    document.querySelector('#add-container>div>h2').firstChild.replaceWith('Add New Card');
-});
-
-// Marker
-etyma_marker.addEventListener('click', () => {
-    let data = cardsData[currentActiveCard];
-    if (typeof(data.marker) != "undefined" && data.marker) {
-        etyma_marker.classList.remove('btn-marker');
-        data.marker = false;
-    } else {
-        etyma_marker.classList.add('btn-marker');
-        data.marker = true;
-    }
-    // 存储数据，不刷新页面
-    localStorage.setItem('cards', JSON.stringify(cardsData));
-});
 
 // 收藏了才加颜色，否则是纯白色
 function etymaMarker(data) {
@@ -326,7 +261,94 @@ function etymaMarker(data) {
     }
 }
 
+// List a single card in DOM
+function listCard(data) {
+    const card = document.createElement('div');
+    card.classList.add('etyma');
+    card.innerHTML = `
+    <h3 class="etyma-title">${data.id}.&nbsp;${data.name}</h3>
+    <p class="etyma-text">
+    ${data.name}:&nbsp;${data.value}<br />
+    ${data.example.name}:&nbsp;${data.example.value}
+    </p>
+    <button class="etyma-toggle">
+    <i class="fa-chevron-down"></i>
+    <i class="fa-times"></i>
+    </button>
+    `;
 
+    // Add to DOM cards
+    cardsElAll.push(card);
+
+    etymaContainer.append(card);
+}
+
+// List All Cards
+function listAllCards(filter, select) {
+    let filterData;
+
+    if (select) {
+        let ids = filter.split('-');
+        filterData = cardsData.filter((item) => {
+            let result;
+            if (isNaN(parseInt(ids[0]))) {
+                result = item.name.indexOf(ids[0]) != -1;
+            } else {
+                if (ids.length == 1) {
+                    result = (item.id == ids[0]);
+                } else {
+                    result = parseInt(item.id) >= parseInt(ids[0]) &&
+                        parseInt(item.id) <= parseInt(ids[1]);
+                }
+            }
+            if (select == 'mark') {
+                result = (item.marker??false) && result;
+            } else if (select == 'unmark') {
+                result = !(item.marker??false) && result;
+            }
+            return result;
+        });
+    } else {
+        filterData = cardsData;
+    }
+    filterData.forEach((data) => listCard(data));
+    const etyma_toggles = document.querySelectorAll('.etyma-toggle');
+    etyma_toggles.forEach(toggle =>
+        toggle.addEventListener('click', () => toggle.parentNode.classList.toggle('active')));
+    foldCards.addEventListener('click', () => {
+        etyma_toggles.forEach(toggle => toggle.parentNode.classList.toggle('active'));
+    });
+}
+
+
+
+/*****************************************************************
+ * 事件绑定区
+ *****************************************************************/
+////////////////////////////////////////////////////////////////
+// 数据导入、导出、清空
+// 点击导入外部数据按钮会自动模拟成点击 input 按钮
+document.querySelector('div>span').addEventListener('click', () => {
+    document.querySelector('div>span>input').click();
+});
+
+//  button 导出
+exportCards.addEventListener('click', exportCardsData);
+
+// Clear cards button
+clearBtn.addEventListener('click', () => {
+    if (confirm("你确定要清空所有数据?")) {
+        localStorage.clear();
+        cardsContainer.innerHTML = '';
+        window.location.reload();
+    }
+});
+
+
+////////////////////////////////////////////////////////////////
+// Card 的新增、删除、修改、标记(Marker)
+// Show add container
+showBtn.addEventListener('click', () => addContainer.classList.add('show'));
 // Add new card
 //
 // 大致创建规则如下
@@ -385,11 +407,118 @@ addCardBtn.addEventListener('click', () => {
     }
 });
 
-// 点击导入外部数据按钮会自动模拟成点击 input 按钮
-document.querySelector('div>span').addEventListener('click', () => {
-    document.querySelector('div>span>input').click();
+// Hide add container
+hideBtn.addEventListener('click', () => {
+    addContainer.classList.remove('show')
+    New = true;
+    etyma_name.value = '';
+    etyma_value.value = '';
+    etyma_example_name.value = '';
+    etyma_example_value.value = '';
+    document.querySelector('#add-container>div>h2').firstChild.replaceWith('Add New Card');
 });
 
+// Delete Card
+etyma_delete.addEventListener('click', () => {
+    if (confirm("你确定要删除当前 Card?")) {
+        cardsId.push(cardsData[currentActiveCard].id);
+        cardsContainer.removeChild(cardsEl[currentActiveCard]);
+        cardsEl.splice(currentActiveCard, 1);
+        cardsData.splice(currentActiveCard, 1);
+        if (currentActiveCard > cardsEl.length - 1) {
+            currentActiveCard = cardsEl.length - 1;
+        }
+        cardsEl[currentActiveCard].className = 'card active'
+        updateCurrentText();
+        etymaMarker(cardsData[currentActiveCard]);
+        // 存储数据，不刷新页面
+        localStorage.setItem('cards', JSON.stringify(cardsData));
+        localStorage.setItem('cardsId', JSON.stringify(cardsId));
+    }
+});
+
+
+// Edit Card
+etyma_edit.addEventListener('click', () => {
+    addContainer.classList.add('show');
+    etyma_name.value = cardsData[currentActiveCard].name;
+    etyma_value.value = cardsData[currentActiveCard].value;
+    etyma_example_name.value = cardsData[currentActiveCard].example.name;
+    etyma_example_value.value = cardsData[currentActiveCard].example.value;
+    New = false;
+    document.querySelector('#add-container>div>h2').firstChild.replaceWith('Edit Cards');
+});
+
+// Marker
+etyma_marker.addEventListener('click', () => {
+    let data = cardsData[currentActiveCard];
+    if (typeof(data.marker) != "undefined" && data.marker) {
+        etyma_marker.classList.remove('btn-marker');
+        data.marker = false;
+    } else {
+        etyma_marker.classList.add('btn-marker');
+        data.marker = true;
+    }
+    // 存储数据，不刷新页面
+    localStorage.setItem('cards', JSON.stringify(cardsData));
+});
+
+
+////////////////////////////////////////////////////////////////
+// Card 的导航：向前、向后、指定跳转、随机跳转
+// Prev button
+prevBtn.addEventListener('click', () => {
+    cardsEl[currentActiveCard].className = 'card right';
+
+    currentActiveCard = currentActiveCard - 1;
+
+    if (currentActiveCard < 0) {
+        currentActiveCard = 0;
+    }
+
+    cardsEl[currentActiveCard].className = 'card active';
+
+    updateCurrentText();
+    etymaMarker(cardsData[currentActiveCard]);
+});
+
+// Next button
+nextBtn.addEventListener('click', () => {
+    cardsEl[currentActiveCard].className = 'card left';
+
+    currentActiveCard = currentActiveCard + 1;
+
+    if (currentActiveCard > cardsEl.length - 1) {
+        currentActiveCard = cardsEl.length - 1;
+    }
+
+    cardsEl[currentActiveCard].className = 'card active';
+
+    updateCurrentText();
+    etymaMarker(cardsData[currentActiveCard]);
+});
+
+// 指定位置跳转
+form_submit.addEventListener('submit', e => {
+    e.preventDefault();
+    let value = currentEl.value.split('/');
+    specifyCards(value[0] - 1, value[1]);
+});
+
+
+
+// radom cards
+etyma_radom.addEventListener('click', () => {
+    let first;
+    do {
+        first = Math.floor(Math.random()*cardsEl.length);
+    } while(cardsData[first].marker);
+    specifyCards(first, cardsEl.length);
+});
+
+
+////////////////////////////////////////////////////////////////
+// 按一定规则来显示数据
 //
 etymaFilter.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -404,46 +533,6 @@ etymaFilter.addEventListener('submit', (e) => {
     listAllCards(filter, select);
 });
 
-// Import other card
-function getExtraCardsData(input) {
-    let file = input.files[0];
-
-    let reader = new FileReader();
-
-    reader.readAsText(file);
-    let etymas;
-    reader.onload = function () {
-        etymas = JSON.parse(reader.result);
-        etymas.forEach((data) => {
-            createCard(data);
-            cardsData.push(data);
-            setCardsData(cardsData);
-        });
-    }
-}
-
-// Clear cards button
-clearBtn.addEventListener('click', () => {
-    if (confirm("你确定要清空所有数据?")) {
-        localStorage.clear();
-        cardsContainer.innerHTML = '';
-        window.location.reload();
-    }
-});
-
-// Export cards button
-exportCards.addEventListener('click', () => {
-    let link = document.createElement('a');
-    link.download = 'etyma.json';
-
-    let blob = new Blob([JSON.stringify(cardsData)], {type: 'text/plain'});
-    link.href = URL.createObjectURL(blob);
-
-    link.click();
-
-    URL.revokeObjectURL(link.href);
-});
-
 
 // 显示全部列表
 // Show add container
@@ -451,68 +540,11 @@ listCards.addEventListener('click', () => etymaContainer.classList.add('show'));
 // Hide add container
 hideCards.addEventListener('click', () => etymaContainer.classList.remove('show'));
 
-// List All Cards
-function listAllCards(filter, select) {
-    let filterData;
-
-    if (select) {
-        let ids = filter.split('-');
-        filterData = cardsData.filter((item) => {
-            let result;
-            if (isNaN(parseInt(ids[0]))) {
-                result = item.name.indexOf(ids[0]) != -1;
-            } else {
-                if (ids.length == 1) {
-                    result = (item.id == ids[0]);
-                } else {
-                    result = parseInt(item.id) >= parseInt(ids[0]) &&
-                        parseInt(item.id) <= parseInt(ids[1]);
-                }
-            }
-            if (select == 'mark') {
-                result = (item.marker??false) && result;
-            } else if (select == 'unmark') {
-                result = !(item.marker??false) && result;
-            }
-            return result;
-        });
-    } else {
-        filterData = cardsData;
-    }
-    filterData.forEach((data) => listCard(data));
-    const etyma_toggles = document.querySelectorAll('.etyma-toggle');
-    etyma_toggles.forEach(toggle =>
-        toggle.addEventListener('click', () => toggle.parentNode.classList.toggle('active')));
-    foldCards.addEventListener('click', () => {
-        etyma_toggles.forEach(toggle => toggle.parentNode.classList.toggle('active'));
-    });
-}
-
-// List a single card in DOM
-function listCard(data) {
-    const card = document.createElement('div');
-    card.classList.add('etyma');
-    card.innerHTML = `
-    <h3 class="etyma-title">${data.id}.&nbsp;${data.name}</h3>
-    <p class="etyma-text">
-    ${data.name}:&nbsp;${data.value}<br />
-    ${data.example.name}:&nbsp;${data.example.value}
-    </p>
-    <button class="etyma-toggle">
-    <i class="fa-chevron-down"></i>
-    <i class="fa-times"></i>
-    </button>
-    `;
-
-    // Add to DOM cards
-    cardsElAll.push(card);
-
-    etymaContainer.append(card);
-}
 
 /***************************************************************
  ** 主程序
  ****************************************************************/
+createCards();
 listAllCards();
 if (typeof(cardsData[0].marker) != "undefined" && (cardsData[0].marker)) {
     etyma_marker.classList.add('btn-marker');
